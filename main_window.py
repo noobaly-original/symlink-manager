7c7b9918223ebdd036ac7374b5d517fa3b7ff7bd
noobaly-original <yigitkomur17@gmail.com>
2026-07-12 02:35:56 +0300

    Bump version in settings GUI to v1.1.2

--- main_window.py
+++ main_window.py
@@ -891,7 +891,7 @@ class SymlinkMainWindow(QMainWindow):
         info_text.setWordWrap(True)
         info_text.setStyleSheet("font-size: 10pt; line-height: 1.5;")
         info_text.setText(
-            f"<b>Symlink Manager v1.1.1</b><br>"
+            f"<b>Symlink Manager v1.1.2</b><br>"
             f"<b>Platform:</b> {self.get_platform_name()}<br>"
             f"<b>Python:</b> {sys.version.split()[0]}"
         )
